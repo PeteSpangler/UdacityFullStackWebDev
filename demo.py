ebe8a8336bd62753cfc2e137372a8e981f225df5
Pete Spangler <63021506+PeteSpangler@users.noreply.github.com>
2020-08-24 15:15:39 -0700

    setting up database changes

--- demo.py
+++ demo.py
@@ -4,6 +4,8 @@ connection = psycopg2.connect('dbname=sandwich user=postgres password=balto')
 
 cursor = connection.cursor()
 
+cursor.execute('DROP TABLE IF EXISTS table2;')
+
 cursor.execute('''
     CREATE TABLE table2(
         id INTEGER PRIMARY KEY,
@@ -12,6 +14,16 @@ cursor.execute('''
 ''')
 
 cursor.execute('INSERT INTO table2 (id, completed) VALUES (1, true);')
+
+cursor.execute('INSERT INTO table2 (id, completed)' + 'VALUES (%(id)s, %(completed)s);', {
+    'id': 2,
+    'completed': False
+})
+
+cursor.execute('SELECT * from table2;')
+
+result = cursor.fetchall()
+print(result)
 
 connection.commit()
 
