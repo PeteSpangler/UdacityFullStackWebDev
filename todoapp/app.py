8d6732b30edd4bc044d5332b97d189382249161c
Pete Spangler <63021506+PeteSpangler@users.noreply.github.com>
2020-09-10 16:56:30 -0700

    immutable bug squashed with utilizing body variable assignment instead of json parsing (I think that is what got it working?)

--- todoapp/app.py
+++ todoapp/app.py
@@ -1,3 +1,4 @@
+import sys
 from flask import Flask, render_template, request, redirect, url_for, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
@@ -17,13 +18,22 @@ db.create_all()
 
 @app.route('/todos/create', methods=['POST'])
 def create_todo():
-    description = request.form.get_json()['description']
-    todo = Todo(description=description)
-    db.session.add(todo)
-    db.session.commit()
-    return jsonify({
-        'description': todo.description
-    })
+    error = False
+    body = {}
+    try:
+        description = request.get_json()['description']
+        todo = Todo(description=description)
+        db.session.add(todo)
+        db.session.commit()
+        body['description'] = todo.description
+    except:
+        error = True
+        db.session.rollback()
+        print(sys.exc_info())
+    finally:
+        db.session.close()
+    if not error:
+        return jsonify(body)
 
 @app.route('/')
 def index():
