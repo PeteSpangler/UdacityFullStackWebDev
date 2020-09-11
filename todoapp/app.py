77d16c47839aa40ee6bd32148522e09e6b17f4f6
Pete Spangler <63021506+PeteSpangler@users.noreply.github.com>
2020-09-11 12:08:53 -0700

    Moved files around, working on flask db init and db migrate

--- todoapp/app.py
+++ todoapp/app.py
@@ -1,10 +1,13 @@
 import sys
 from flask import Flask, render_template, request, redirect, url_for, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:balto@localhost:5432/todoapp'
 db = SQLAlchemy(app)
+
+migrate = Migrate(app, db)
 
 class Todo(db.Model):
     __tablename__ = 'todos'
@@ -14,7 +17,6 @@ class Todo(db.Model):
     def __repr__(self):
         return f'<Todo {self.id} {self.description}>'
 
-db.create_all()
 
 @app.route('/todos/create', methods=['POST'])
 def create_todo():
