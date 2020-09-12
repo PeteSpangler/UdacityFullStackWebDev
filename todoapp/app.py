eeaac2afd4abeb8a4d78a3f3bccf2ee2992a7b69
Pete Spangler <63021506+PeteSpangler@users.noreply.github.com>
2020-09-12 14:53:50 -0700

    Finishing up the Todoapp practice set

--- todoapp/app.py
+++ todoapp/app.py
@@ -1,23 +1,33 @@
 import sys
-from flask import Flask, render_template, request, redirect, url_for, jsonify
+from flask import Flask, render_template, request, redirect, url_for, jsonify, abort
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:balto@localhost:5432/todoapp'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 migrate = Migrate(app, db)
+
+class TodoList(db.Model):
+    __tablename__ = 'todolists'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(), nullable=False)
+    todos = db.relationship('Todo', backref='list', cascade='all, delete-orphan', lazy=True)
+
+    def __repr__(self):
+        return f'<TodoList {self.id} {self.name}>'
 
 class Todo(db.Model):
     __tablename__ = 'todos'
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(), nullable=False)
-    completed = db.Column(db.Boolean(), nullable=False, default=False)
-
+    completed = db.Column(db.Boolean, nullable=False, default=False)
+    list_id = db.Column(db.Integer, db.ForeignKey('todolists.id'), nullable=False)
+    
     def __repr__(self):
-        return f'<Todo {self.id} {self.description}>'
-
+        return f'<Todo {self.id} {self.description}, list {self.list_id}>'
 
 @app.route('/todos/create', methods=['POST'])
 def create_todo():
@@ -25,9 +35,11 @@ def create_todo():
     body = {}
     try:
         description = request.get_json()['description']
-        todo = Todo(description=description)
+        todo = Todo(description=description, completed=False)
         db.session.add(todo)
         db.session.commit()
+        body['id'] = todo.id
+        body['completed'] = todo.completed
         body['description'] = todo.description
     except:
         error = True
@@ -35,13 +47,16 @@ def create_todo():
         print(sys.exc_info())
     finally:
         db.session.close()
-    if not error:
+    if error:
+        abort (400)
+    else:
         return jsonify(body)
 
 @app.route('/todos/<todo_id>/set-completed', methods=['POST'])
 def set_completed_todo(todo_id):
     try:
         completed = request.get_json()['completed']
+        print('completed', completed)
         todo = Todo.query.get(todo_id)
         todo.completed = completed
         db.session.commit()
@@ -60,8 +75,13 @@ def delete_todo(todo_id):
         db.session.rollback()
     finally:
         db.session.close()
-    return jsonify({ 'success': True})
+    return jsonify({ 'success': True })
+
+@app.route('/lists/<list_id>')
+def get_list_todos(list_id):
+    return render_template('index.html', 
+    data=Todo.query.filter_by(list_id=list_id).order_by('id').all())
 
 @app.route('/')
 def index():
-    return render_template('index.html', data=Todo.query.order_by('id').all())
+    return redirect(url_for('get_list_todos', list_id=1))
